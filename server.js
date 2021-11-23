50e0121302a392ed078dfae61c522437faf291e7
Matthew Dwyer <matthewgdwyer@gmail.com>
2021-11-23 13:59:15 +0700

    restructured file tree

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.use(express.static(__dirname + "/styles.css"));

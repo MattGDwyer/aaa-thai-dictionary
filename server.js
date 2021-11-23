5fd3fb353ea01b748fd5bf2ebe3d49bd98cbf788
Matthew Dwyer <matthewgdwyer@gmail.com>
2021-11-23 14:08:38 +0700

    adjusting file tree

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ var PORT = process.env.PORT || 3000;
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
-});
+  }
+);
 
-app.use(express.static(__dirname + "/styles.css"));
+app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/styles.css"));
 
 app.use(express.static(__dirname));
 
